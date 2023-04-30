d01fff949dc8218b31f7a02a306a019117f53f18
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-04-30 02:51:53 -0400

    completed context in JS

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
  for you to use if you need it!
  */
 
-const allWagesFor = function () {
+ const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
     })
@@ -20,4 +20,53 @@ const allWagesFor = function () {
 
     return payable
 }
+function createEmployeeRecord(employeeData) {
+    return {
+      firstName: employeeData[0],
+      familyName: employeeData[1],
+      title: employeeData[2],
+      payPerHour: employeeData[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    }
+  }
+  function createEmployeeRecords(records) {
+    return records.map(createEmployeeRecord);
+  }
+    function createTimeInEvent(dateStamp) {
+        const [date, hour] = dateStamp.split(" ");
+        this.timeInEvents.push({ type: "TimeIn", date, hour: parseInt(hour, 10) });
+        return this;
+      }
+      function createTimeOutEvent(dateStamp) {
+        const [date, hour] = dateStamp.split(" ");
+        this.timeOutEvents.push({ type: "TimeOut", date, hour: parseInt(hour, 10) });
+        return this;
+      }
+      function hoursWorkedOnDate(date) {
+        const timeInEvent = this.timeInEvents.find(event => event.date === date);
+        if (!timeInEvent) {
+          return 0;
+        }
+      
+        const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+        if (!timeOutEvent) {
+          return 0;
+        }
+      
+        const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+        return hoursWorked;
+      } 
+      function wagesEarnedOnDate(date) {
+        const hoursWorked = hoursWorkedOnDate.call(this, date);
+        const totalWages = hoursWorked * this.payPerHour;
+        return totalWages;
+      }
+      function findEmployeeByFirstName(employees, firstName) {
+        return employees.find((employee) => employee.firstName === firstName);
+      }
+      function calculatePayroll(employees) {
+        const allWages = employees.map(employee => allWagesFor.call(employee));
+        return allWages.reduce((totalWages, wages) => totalWages + wages, 0);
+      }
 
